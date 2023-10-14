3dcd6b8a68e8fc168bae863212763a0522943529
Ellen <78161924+EllenJP@users.noreply.github.com>
2023-10-15 03:58:26 +0900

    tmp

--- lib/src/seen/talking.dart
+++ lib/src/seen/talking.dart
@@ -69,99 +69,52 @@ class _TalkScreenState extends State<TalkScreen> {
   @override
   Widget build(BuildContext context) {
     final GlobalKey<ScaffoldState> _scaffoldKey = GlobalKey<ScaffoldState>();
-    // UnityWidgetController? _unityWidgetController;
 
     return Scaffold(
-      body: Row(
-        children: [
-          // Live2Dはここから
-          Stack(children: <Widget>[
-            UnityWidget(
-              onUnityCreated: onUnityCreated,
-              onUnityMessage: onUnityMessage,
-              onUnitySceneLoaded: onUnitySceneLoaded,
-              fullscreen: false,
+        body: Stack(
+      children: <Widget>[
+        UnityWidget(
+          onUnityCreated: onUnityCreated,
+          onUnityMessage: onUnityMessage,
+          onUnitySceneLoaded: onUnitySceneLoaded,
+          fullscreen: false,
+        ),
+        Container(
+          width: 550,
+          height: 100,
+          decoration: BoxDecoration(
+            color: Color(0xFFC51162).withOpacity(0.5), // 背景色を指定
+            borderRadius: BorderRadius.circular(8.0), // 角を丸くする半径を指定
+            border: Border.all(
+              color: Colors.pink, // ボーダーの色を設定
+              width: 1.0, // ボーダーの幅を設定
             ),
-            // Live2Dはここまで
-            Column(
-              mainAxisAlignment: MainAxisAlignment.end,
-              children: [
-                SizedBox(
-                  height: 20,
-                  width: 10,
-                ),
-                Center(
-                  child: Container(
-                    width: 630,
-                    height: 100,
-                    decoration: BoxDecoration(
-                      color: Color(0xFFC51162).withOpacity(0.5), // 背景色を指定
-                      borderRadius: BorderRadius.circular(8.0), // 角を丸くする半径を指定
-                      border: Border.all(
-                        color: Colors.pink, // ボーダーの色を設定
-                        width: 1.0, // ボーダーの幅を設定
-                      ),
-                    ),
-                    child: Column(
-                      children: [
-                        Align(
-                          alignment: Alignment.centerLeft, // テキストを右寄せに配置
-                          child: flag == 0 ? Text('あなた') : Text('桃瀬ひより'),
-                        ),
-                        SizedBox(
-                          height: 5,
-                          width: 10,
-                        ),
-                        Align(
-                          alignment: Alignment.centerLeft,
-                          child: Text('$text',
-                              overflow: TextOverflow.ellipsis,
-                              maxLines: 3,
-                              style: const TextStyle(
-                                fontSize: 15,
-                                fontFamily: 'Klee_One',
-                              )),
-                        ),
-                      ],
-                    ),
-                  ),
-                ),
-              ],
-            ),
-          ]),
-        ],
-      ),
-      floatingActionButtonLocation: FloatingActionButtonLocation.endFloat,
-      floatingActionButton: FloatingActionButton(
-        onPressed: isDisabled
-            ? null
-            : () async {
-                // マイクボタンが押されたときに音声認識を開始
-                flag = 0;
-                isDisabled = true;
-                Voiceget();
-                if (sessionflag == 0) {
-                  sessionflag = 1;
-                  int session_time =
-                      await sessiontime_get_Http(widget.username);
-                  print("session_time:$session_time");
-                }
-                await Future.delayed(Duration(seconds: 5));
-                String result = await _post_request(text, session_time, count);
-                count++;
-                setState(() {
-                  text = result;
-                });
-                flag = 1;
-                _speak(text);
-
-                final getlist = await WordList_get_Http('1');
-                setState(() => wordList = getlist);
-                isDisabled = false;
-              },
-        child: Icon(Icons.mic),
-      ),
-    );
+          ),
+          child: Column(
+            children: [
+              Align(
+                alignment: Alignment.centerLeft, // テキストを右寄せに配置
+                child: flag == 0 ? Text('あなた') : Text('桃瀬ひより'),
+              ),
+              SizedBox(
+                height: 5,
+                width: 10,
+              ),
+              Align(
+                alignment: Alignment.centerLeft,
+                child: Text('$text',
+                    overflow: TextOverflow.ellipsis,
+                    maxLines: 3,
+                    style: const TextStyle(
+                      fontSize: 15,
+                      fontFamily: 'Klee_One',
+                    )),
+              ),
+            ],
+          ),
+        ),
+      ],
+    ));
   }
 
   Future<void> Voiceget() async {
